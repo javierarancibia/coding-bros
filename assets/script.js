81eaad2c1c9d464ba17e0af04fbf58618ac7db40
Javier <javier.arancibia.reyes@hotmail.com>
2021-04-13 14:35:25 -0400

    commit

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+let we = document.getElementById('nosotros');
+we.addEventListener("click", nosotros)
+
+
+function nosotros() {
+    $('#montaje').fadeOut(1000)
+    $('#we').fadeIn(8500).css('left' ,'0');
+}
+
