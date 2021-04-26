254e2bcd034aff018ff5a761d0916d77d180c8ca
Javier <javier.arancibia.reyes@hotmail.com>
2021-04-26 16:36:27 -0400

    added portfolio

--- assets/script.js
+++ assets/script.js
@@ -11,7 +11,4 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
   }
   
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
 
